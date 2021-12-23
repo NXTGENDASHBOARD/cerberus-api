395cd37711565ae5173753d26a090f05866a8c25
Freedom Nduduzo Khanyile <freedom.khanyile@adaptit.com>
2021-12-23 11:57:23 +0200

    Created end point modal and command to handle local database user account

---
+++ Cerberus.Dashboard.Api/DTOs/LoginDTO.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Cerberus.Dashboard.Api.DTOs
+{
+    public class LoginDTO
+    {
+        [Required]
+        public string StaffNumber { get; set; }
+
+        [Required]
+        public int Pin { get; set; }
+    }
+
+    public class LoginResponseDto
+    {
+        public string Title { get; set; }
+        public string FullName { get; set; }
+        public string Surname { get; set; }
+        public string Status { get; set; }
+
+        [JsonIgnore]
+        public string Token { get; set; }
+
+        public List<RoleDTO> Roles { get; set; }
+    }
+}
---
+++ Cerberus.Dashboard.Api/DTOs/RoleDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Cerberus.Dashboard.Api.DTOs
+{
+    public class RoleDTO
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string AccessLevel { get; set; }
+
+        [JsonIgnore]
+        public Dictionary<string,string> SourceList { get; set; }
+    }
+}
