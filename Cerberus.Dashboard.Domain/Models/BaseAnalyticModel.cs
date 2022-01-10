5213e2d11f7700d46cbf495b6fe9fe6c514730e6
mahakata_a <andrewm@praxis.co.za>
2022-01-10 09:34:43 +0200

    Created Models

---
+++ Cerberus.Dashboard.Domain/Models/BaseAnalyticModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Cerberus.Dashboard.Domain.Models
+{
+    public abstract class BaseAnalyticModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public int Sum { get; set; }
+
+        public DateTime DateRecord { get; set; }
+        public int LastSum { get; set; }
+        public DateTime LastDateRecord { get; set; }
+        public string AnalyticType { get; set; }
+
+
+
+    }
+}
---
+++ Cerberus.Dashboard.Domain/Models/CourseTypeApplicationAnalytic.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cerberus.Dashboard.Domain.Models
+{
+    public class CourseTypeApplicationAnalytic : BaseAnalyticModel
+    {
+    }
+}
