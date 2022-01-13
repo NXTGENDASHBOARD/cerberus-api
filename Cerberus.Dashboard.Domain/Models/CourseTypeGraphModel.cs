d737b223acea4095d11aa9abf3383584e0c08c95
mahakata_a <andrewm@praxis.co.za>
2022-01-13 12:46:08 +0200

    Added changes for the CourseType Graph

---
+++ Cerberus.Dashboard.Domain/Models/CourseTypeGraphModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cerberus.Dashboard.Domain.Models
+{
+    public class CourseTypeGraphModel : BaseGraphModel
+    {
+        public string Course { get; set; }
+        public List<Series> CourseSeries { get; set; }
+
+        public class Series
+        {
+            public string Name { get; set; }
+            public int Value { get; set; }
+        }
+    }
+}
